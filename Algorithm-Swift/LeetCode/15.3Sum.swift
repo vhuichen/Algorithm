ae139a28715dc213ec21dc94995734b2127e65c2
chenhui <vhuichen@163.com>
2021-01-06 19:39:10 +0800

    18. 四数之和

--- Algorithm-Swift/LeetCode/15.3Sum.swift
+++ Algorithm-Swift/LeetCode/15.3Sum.swift
@@ -17,33 +17,21 @@ extension Solution {
         
         var val = [[Int]]()
         var first = 0
-        let firstNumMax = numsSort.count - 2
+        let firstNumLimit = numsSort.count - 2
         let thirdNumMax = numsSort.count - 1
         
-        while first < firstNumMax {
-            if numsSort[first] > 0 { break }
-            
+        while first < firstNumLimit {
             var second = first + 1
             var third = thirdNumMax
             let target = -numsSort[first]
             
             while second < third {
-                if numsSort[second] > target { break }
-                
                 let sum = numsSort[second] + numsSort[third]
                 
                 if sum > target {
-                    
-                    repeat {
-                        third -= 1
-                    } while second < third && numsSort[third] == numsSort[third + 1]
-                    
+                    third -= 1
                 } else if sum < target {
-                    
-                    repeat {
-                        second += 1
-                    } while second < third && numsSort[second - 1] == numsSort[second]
-                    
+                    second += 1
                 } else {
                     val.append([numsSort[first], numsSort[second], numsSort[third]])
                     
@@ -60,7 +48,7 @@ extension Solution {
             
             repeat {
                 first += 1
-            } while first < firstNumMax && numsSort[first - 1] == numsSort[first]
+            } while first < firstNumLimit && numsSort[first - 1] == numsSort[first]
         }
         
         return val
---
+++ Algorithm-SwiftTests/LeetCodeTests/16.3SumClosestTests.swift
@@ -0,0 +1,55 @@
+//
+//  16.swift
+//  Algorithm-SwiftTests
+//
+//  Created by chenhui on 2021/1/6.
+//  Copyright © 2021 vhuichen. All rights reserved.
+//
+
+import XCTest
+@testable import Algorithm_Swift
+
+class ThreeSumClosestTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let solution = Solution()
+        var s = [-1, 0, 1, 2, -1, -4]
+        var result = solution.threeSumClosest(s, 0)
+        XCTAssert(result == 0)
+        
+        s = [-1, 0, 1, 2, -1, -4]
+        result = solution.threeSumClosest(s, 10)
+        XCTAssert(result == 3)
+        
+        s = [-1,2,1,-4]
+        result = solution.threeSumClosest(s, 1)
+        XCTAssert(result == 2)
+        
+        s = [1,1,-1,-1,3]
+        result = solution.threeSumClosest(s, 1)
+        XCTAssert(result == 1)
+        
+        s = [1,1,-1,-1,3]
+        result = solution.threeSumClosest(s, 3)
+        XCTAssert(result == 3)
+    }
+    
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+            
+        }
+    }
+
+}
