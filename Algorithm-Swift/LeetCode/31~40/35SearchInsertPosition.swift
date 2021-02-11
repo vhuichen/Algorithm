df7e5b6ad75aba924a2918a83f708e29097e2218
vchan <vhuichen@163.com>
2021-02-11 11:44:59 +0800

    36 有效的数独

---
+++ Algorithm-Swift/LeetCode/31~40/35SearchInsertPosition.swift
@@ -0,0 +1,28 @@
+//
+//  35SearchInsertPosition.swift
+//  Algorithm-Swift
+//
+//  Created by chenhui on 2021/2/10.
+//
+
+import Foundation
+
+extension Solution {
+    func searchInsert(_ nums: [Int], _ target: Int) -> Int {
+        var left = 0
+        var right = nums.count - 1
+        var index = nums.count
+        
+        while left <= right {
+            let mid = (left + right) / 2
+            if nums[mid] < target {
+                left = mid + 1
+            } else {
+                index = mid
+                right = mid - 1
+            }
+        }
+        
+        return index
+    }
+}
