673e8b5a15d3185c6f88f5916d5989062c996fd6
vchan <vhuichen@163.com>
2021-03-08 00:38:09 +0800

    add

--- Algorithm/LeetCode/121BestTimeToBuyAndSellStock.swift
+++ Algorithm/LeetCode/121BestTimeToBuyAndSellStock.swift
@@ -117,3 +117,20 @@ extension Solution {
         return results
     }
 }
+
+
+extension Solution {
+    func rob(_ nums: [Int]) -> Int {
+        guard nums.count > 1 else { return nums[0] }
+        
+        var dp = [Int](repeating: 0, count: nums.count)
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+        
+        for i in 2..<nums.count {
+            dp[i] = max(dp[i - 2] + nums[i], dp[i - 1])
+        }
+        
+        return dp.last!
+    }
+}
